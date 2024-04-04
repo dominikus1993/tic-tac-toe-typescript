ee8e003cb5c7b14f5a0b257058e78f1b75b76842
Dominik.Kotecki <dominikus1910@gmail.com>
2024-04-04 18:17:08 +0200

    add tests

--- src/TicTacToe/Core/Boards/Board.cs
+++ src/TicTacToe/Core/Boards/Board.cs
@@ -7,16 +7,31 @@ namespace TicTacToe.Core.Boards;
 public sealed class Board
 {
     private readonly Cell[,] _cells;
-    
+    private readonly int _size;
     public Player? CheckWinner() => throw new System.NotImplementedException();
-    
-    public bool IsFull() => throw new System.NotImplementedException();
+
+    public bool IsFull()
+    {
+        foreach (var cell in _cells)
+        {
+            if (cell.IsEmpty())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
     
     private Board(Cell[,] cells)
     {
         _cells = cells;
+        _size = cells.GetLength(0);
     }
 
+    public bool IsMoveValid(int x, int y)
+    {
+        return x < _size && y < _size && _cells[x, y].IsEmpty();
+    }
 
 
     public static Board Create(int n = 3)
